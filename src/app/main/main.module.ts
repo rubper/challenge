fe0fab2f2917be7de04f2580d6336ddb13dd4b0e
Rubén Pérez <49503500+rubper@users.noreply.github.com>
2021-06-20 07:11:38 -0600

    index

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,3 +1,4 @@
+import { MatCardModule } from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -33,6 +34,7 @@ import { IndexComponent } from './index/index.component';
     ReactiveFormsModule,
     MatCheckboxModule,
     MatSelectModule,
+    MatCardModule,
     MatIconModule
   ]
 })
